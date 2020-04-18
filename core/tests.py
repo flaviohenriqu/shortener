a7dba079f7313a06ae544eae07a348a3424ec049
Flavio Henrique <flaviohenriqu@gmail.com>
2020-04-18 15:47:48 +0100

    test(tests): add unit tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from rest_framework.test import APIClient, APITestCase
 
-# Create your tests here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+class URLShortenerTest(APITestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = APIClient()
+        # create user
+        response = self.client.post(
+            reverse('users:create'),
+            {
+                "username": "john",
+                "email": "user@test.com",
+                "password": "smith",
+                "confirm_password": "smith",
+                "date_joined": "2020-04-17T00:00"
+            }
+        )
+        self.token = response.json()['access']
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+
+    def test_url_short_invalid(self):
+        response = self.client.post(reverse('core:list'), {'full_url': 'google.com'})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'full_url': ['Enter a valid URL.']})
+
+    def test_url_short_successful(self):
+        response = self.client.post(reverse('core:list'), {'full_url': 'https://www.google.com'})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['full_url'], 'https://www.google.com')
+
+    def test_redirect_short_url(self):
+        response = self.client.post(reverse('core:list'), {'full_url': 'https://www.google.com'})
+        self.assertEqual(response.status_code, 201)
+        slug = response.json()['slug']
+
+        response_redirect = self.client.get(reverse('core:redirect', kwargs={'slug': f'{slug}'}))
+        self.assertEqual(response_redirect.status_code, 302)
+        self.assertEqual(type(response_redirect), HttpResponseRedirect)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserLoginTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        # create user
+        self.client.post(
+            reverse('users:create'),
+            {
+                "username": "john",
+                "email": "user@test.com",
+                "password": "smith",
+                "confirm_password": "smith",
+                "date_joined": "2020-04-17T00:00"
+            }
+        )
+
+    def test_login_sucessful(self):
+        response = self.client.post(reverse('users:token_obtain_pair'), {'username': 'john', 'password': 'smith'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'access')
+
+    def test_login_fail(self):
+        response = self.client.post(reverse('users:token_obtain_pair'), {'username': 'john', 'password': 'smith1'})
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.content, b'{"detail":"No active account found with the given credentials"}')
+
+    def test_refresh_token(self):
+        response = self.client.post(reverse('users:token_obtain_pair'), {'username': 'john', 'password': 'smith'})
+        refresh_token = response.json()['refresh']
+        token = response.json()['access']
+        refresh_response = self.client.post(
+            reverse('users:token_refresh'),
+            {'refresh': f'{refresh_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(refresh_response.json()['access'], token)
